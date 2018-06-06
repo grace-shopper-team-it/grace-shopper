603b6265ade98b6f29b31bc496c1858650d54e96
aurre <aurrecochearaysa@gmail.com>
2018-06-06 16:12:04 -0500

    more changes

--- server/api/products.js
+++ server/api/products.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const { Product } = require('../db/models');
 const { Review } = require('../db/models');
+const { Category } = require('../db/models');
 
 module.exports = router;
 
 // Get all the products
 router.get('/', (req, res, next) => {
-  Product.findAll()
+  Product.findAll({ include: [Category] })
     .then(products => {
       res.json(products);
     })
