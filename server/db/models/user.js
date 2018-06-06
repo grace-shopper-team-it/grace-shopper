dd396b448c118a8d197beca80f5bb709e442cd13
Drevets <drevets@gmail.com>
2018-06-06 11:32:37 -0500

    removed null constraints on names to make it easier to log in with Google

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -5,17 +5,9 @@ const db = require('../db')
 const User = db.define('user', {
   firstName: {
     type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
   },
   lastName: {
     type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
   },
   email: {
     type: Sequelize.STRING,
