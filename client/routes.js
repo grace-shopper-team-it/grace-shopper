44d743b7bd2d483924162b0b6badd11c93fd11ed
aurre <aurrecochearaysa@gmail.com>
2018-06-07 13:40:20 -0500

    small change

--- client/routes.js
+++ client/routes.js
@@ -37,9 +37,8 @@ class Routes extends Component {
         <Route exact path="/products/new" component={AddProduct} />
         <Route path="/allProducts" component={AllProducts} />
         <Route path="/products/:id/edit" component={EditProduct} />
-        <Route path="/products/:id/review" />
+        <Route path="/products/:id/review" component={AddReview} />
         <Route path="/products/:id" component={SingleProduct} />
-        <Route exact path="/products/:id/review" component={AddReview} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
