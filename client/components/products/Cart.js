87c680e25a72a3a8c8ca79d3f43892cc8fefa459
Chris <tophersjones@gmail.com>
2018-06-06 20:30:17 -0500

    commit

--- client/components/products/Cart.js
+++ client/components/products/Cart.js
@@ -1,86 +1,41 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 
-let items = [
-  {
-    id: 1,
-    name: 'clown',
-    rating: 3.5,
-    description: 'eiugrh fIOWAHGUIRW ofejhguieroils',
-    imageUrl:
-      'https://images-na.ssl-images-amazon.com/images/I/41PKN8W5CDL.jpg',
-    stock: 5,
-    price: 30,
-    category: 'creepy',
-  },
-  {
-    id: 2,
-    name: 'nice clown',
-    rating: 3.0,
-    description: 'eiugrh fIOWAHGUIRW',
-    imageUrl: 'https://i.ebayimg.com/images/g/LFUAAOSwKtlWjzVd/s-l300.jpg',
-    stock: 3,
-    price: 20,
-    category: 'creepy',
-  },
-  {
-    id: 3,
-    name: 'nice toy',
-    rating: 2.0,
-    description: 'eiugrh fIOWAHGUIRW',
-    imageUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbZZNPfu7n3fI5Zygu0qGXc1mtuXYGCD2oNGq-WxZ3hMBhYbhR',
-    stock: 3.8,
-    price: 25,
-    category: 'nice',
-  },
-];
-
-export class Cart extends Component {
-  constructor() {
-    super()
-    this.state = {
-      ok: 'ok'
-    }
-  }
-
-  componentDidMount () {
-    try {
-      this.props.fetchCart()
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  render() {
-    const ok = this.state.ok
-    console.log(ok)
-    console.log('testTEwst')
-    return (
-      <div>
-        <h1> Cart </h1>
-        <h3>{items[0]}</h3>
-      </div>
-    )
-  }
+const Cart = (props) => {
+  console.log('testTEwst')
+  const { items } = props
+  return (
+    <div>
+      <h1> Cart </h1>
+      {
+        props.map((product) => {
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+          </div>
+        })
+      }
+    </div>
+  )
 }
 
-const mapState = state => {
+
+const mapState = (state) => {
   return {
     cart: state.cart
   }
 }
 
-const mapDispatchToProps = function (dispatch) {
-  return {
-    fetchInitialCart: () => {
-      dispatch(fetchCart())
-    }
-  }
-}
+// const mapDispatchToProps = function (dispatch) {
+//   return {
+//     fetchInitialCart: () => {
+//       dispatch(fetchCart())
+//     }
+//   }
+// }
 
 export default connect(
   mapState,
-  mapDispatchToProps
+  null
 )(Cart)
--- client/store/cart.js
+++ client/store/cart.js
@@ -1,5 +1,3 @@
-import axios from 'axios'
-
 /*
   action types
 */
@@ -20,7 +18,7 @@ const addToCartAction = (product, quantity) => {
   }
 }
 
-const removeFromCartAction = product => {
+const removeFromCartAction = (product) => {
   return {
     type: REMOVE_FROM_CART,
     product
@@ -33,29 +31,67 @@ const clearCartAction = () => {
   }
 }
 
-const updateQuantity = amount => {
+const updateQuantity = (quantity) => {
   return {
     type: UPDATE_QUANTITY,
-    amount
+    quantity
   }
 }
 
-export const fetchCart() {
-  
-}
-
 const initialState = {
-  cart: []
+  //dummy data
+  cart: [ {
+    id: 1,
+    name: 'clown',
+    rating: 3.5,
+    description: 'eiugrh fIOWAHGUIRW ofejhguieroils',
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/41PKN8W5CDL.jpg',
+    stock: 5,
+    price: 30,
+    category: 'creepy',
+  },
+  {
+    id: 2,
+    name: 'nice clown',
+    rating: 3.0,
+    description: 'eiugrh fIOWAHGUIRW',
+    imageUrl: 'https://i.ebayimg.com/images/g/LFUAAOSwKtlWjzVd/s-l300.jpg',
+    stock: 3,
+    price: 20,
+    category: 'creepy',
+  },
+  {
+    id: 3,
+    name: 'nice toy',
+    rating: 2.0,
+    description: 'eiugrh fIOWAHGUIRW',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbZZNPfu7n3fI5Zygu0qGXc1mtuXYGCD2oNGq-WxZ3hMBhYbhR',
+    stock: 3.8,
+    price: 25,
+    category: 'nice',
+  } ]
 }
 
 export default function (state = initialState, action) {
+  // adding 'quantity' property to products when they are passed to the cart
   switch (action.type) {
-    case ADD_TO_CART:
-      const cartItem = Object.assign({}, )
-      return { ...state, cart: [ ...state.cart, action.product ] }
-    case REMOVE_FROM_CART:
+    case ADD_TO_CART: {
+      const cartItem = Object.assign(action.product, { cartQuantity: action.quantity })
+      return { ...state, cart: [ ...state.cart, cartItem ] }
+    }
+    case UPDATE_QUANTITY: {
+    // find product to update, creates new obj with updated quantity, filters out old obj of state, adds newObj to newArr
+      const objToBeUpdated = state.cart.find((product) => product.id === action.product.id)
+      const newObj = Object.assign(objToBeUpdated, { cartQuantity: action.quantity })
+      const updatedCart = state.cart.filter(product => Number(product.id) !== Number(action.product.id)) 
+      return { ...state, cart: [ ...updatedCart, newObj ]}
+    }
+    case REMOVE_FROM_CART: {
       const updatedCart = state.cart.filter(product => Number(product.id) !== Number(action.product.id))
       return { ...state, cart: updatedCart }
+    }
     case CLEAR_CART:
       return { ...state, cart: [] }
     default:
