d11b8cd42fcfd3cc066d14f265a93699d4440077
aurre <aurrecochearaysa@gmail.com>
2018-06-07 19:01:35 -0500

    review page rendering fine

--- client/components/products/AddReview.js
+++ client/components/products/AddReview.js
@@ -5,7 +5,9 @@ import './AddReview.css';
 const AddReview = () => (
   <form>
     <div className="form-group">
-      <label for="exampleFormControlTextarea1">Content</label>
+      <label htmlFor="exampleFormControlTextarea1">
+        Write your Review Here
+      </label>
       <textarea
         className="form-control"
         id="exampleFormControlTextarea1"
@@ -15,31 +17,39 @@ const AddReview = () => (
     <div className="section">
       <fieldset className="rating">
         <input type="radio" id="star5" name="rating" value="5" />
-        <label className="full" for="star5" title="Awesome - 5 stars" />
+        <label className="full" htmlFor="star5" title="Awesome - 5 stars" />
         <input type="radio" id="star4half" name="rating" value="4 and a half" />
         <label
           className="half"
-          for="star4half"
+          htmlFor="star4half"
           title="Pretty good - 4.5 stars"
         />
         <input type="radio" id="star4" name="rating" value="4" />
-        <label className="full" for="star4" title="Pretty good - 4 stars" />
+        <label className="full" htmlFor="star4" title="Pretty good - 4 stars" />
         <input type="radio" id="star3half" name="rating" value="3 and a half" />
-        <label className="half" for="star3half" title="Meh - 3.5 stars" />
+        <label className="half" htmlFor="star3half" title="Meh - 3.5 stars" />
         <input type="radio" id="star3" name="rating" value="3" />
-        <label className="full" for="star3" title="Meh - 3 stars" />
+        <label className="full" htmlFor="star3" title="Meh - 3 stars" />
         <input type="radio" id="star2half" name="rating" value="2 and a half" />
-        <label className="half" for="star2half" title="Kinda bad - 2.5 stars" />
+        <label
+          className="half"
+          htmlFor="star2half"
+          title="Kinda bad - 2.5 stars"
+        />
         <input type="radio" id="star2" name="rating" value="2" />
-        <label className="full" for="star2" title="Kinda bad - 2 stars" />
+        <label className="full" htmlFor="star2" title="Kinda bad - 2 stars" />
         <input type="radio" id="star1half" name="rating" value="1 and a half" />
-        <label className="half" for="star1half" title="Meh - 1.5 stars" />
+        <label className="half" htmlFor="star1half" title="Meh - 1.5 stars" />
         <input type="radio" id="star1" name="rating" value="1" />
-        <label className="full" for="star1" title="Sucks big time - 1 star" />
+        <label
+          className="full"
+          htmlFor="star1"
+          title="Sucks big time - 1 star"
+        />
         <input type="radio" id="starhalf" name="rating" value="half" />
         <label
           className="half"
-          for="starhalf"
+          htmlFor="starhalf"
           title="Sucks big time - 0.5 stars"
         />
       </fieldset>
