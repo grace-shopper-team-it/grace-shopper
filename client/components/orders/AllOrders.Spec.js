762ed3820974fdabdea859af226c116ff7bf9ea9
Drevets <drevets@gmail.com>
2018-06-09 13:29:26 -0500

    npm install

--- client/components/orders/AllOrders.Spec.js
+++ client/components/orders/AllOrders.Spec.js
@@ -2,13 +2,16 @@ import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import React from 'react';
 import { AllOrders } from './AllOrders';
+import sinon from 'sinon'
 
 //to test for: does this page render a list of orders?
 //how to test for authorization with react components??
 
 describe('AllOrders component', () => {
+  const mockGetOrders = sinon.spy();
+  const mockUpdateOrder = sinon.spy();
   it('renders a list of orders', () => {
     let wrapper;
-    const mockGetOrder = jest.fn();
-  })
+  });
+
 })
