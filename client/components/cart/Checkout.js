16b50a173f1cef7518d8b82451343822e7aa9ca7
Chris <tophersjones@gmail.com>
2018-06-10 18:56:21 -0500

    added form on checkout page

--- client/components/cart/Checkout.js
+++ client/components/cart/Checkout.js
@@ -4,7 +4,16 @@ import { connect } from 'react-redux';
 class Checkout extends Component {
   constructor() {
     super();
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = {
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zipcode: 0,
+      email: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleSubmit(event) {
@@ -13,18 +22,93 @@ class Checkout extends Component {
     this.props.submitOrder();
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     const items = this.props.cart.cart;
     console.log(items);
     return (
-      <h2>Checkout Page</h2>
+      <div>
+        <h2>Checkout Page</h2>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Enter your shipping address</h4>
+          <label>
+            Street 1
+            <input
+              name="street1"
+              type="text"
+              value={this.state.street1}
+              onChange={this.handleInputChange} />
+              (required)
+          </label>
+            <br />
+          <label>
+            Street 2
+            <input
+              name="street2"
+              type="text"
+              value={this.state.street2}
+              onChange={this.handleInputChange} />
+          </label>
+            <br />
+          <label>
+            City
+            <input
+              name="city"
+              type="text"
+              value={this.state.city}
+              onChange={this.handleInputChange} />
+              (required)
+          </label>
+            <br />
+          <label>
+            State
+            <input
+              name="state"
+              type="text"
+              value={this.state.state}
+              onChange={this.handleInputChange} />
+              (required)
+          </label>
+            <br />
+          <label>
+            Zip Code
+            <input
+              name="zipcode"
+              type="text"
+              value={this.state.zipcode}
+              onChange={this.handleInputChange} />
+              (required)
+          </label>
+            <h4>Confirm your email</h4>
+          <label>
+            Email
+            <input
+              name="email"
+              type="text"
+              value={this.state.email}
+              onChange={this.handleInputChange} />
+              (required)
+          </label>
+            <br />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
     );
   }
 }
 
 const mapState = (state) => {
   return {
-    cart: state.cart
+    cart: state.cart,
+
   };
 };
 
