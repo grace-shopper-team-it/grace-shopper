9f07eb0a6d8612cff3986c1e90d451b894d32388
Drevets <drevets@gmail.com>
2018-06-11 16:09:08 -0500

    figuring out stripe

---
+++ client/components/cart/StripePayment.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
+
+export default class TakeMoney extends React.Component {
+  onToken = (token) => {
+    fetch('/save-stripe-token', {
+      method: 'POST',
+      body: JSON.stringify(token),
+    }).then(response => {
+      response.json().then(data => {
+        alert(`We are in business, ${data.email}`);
+      })
+    })
+  }
+
+  //the below renders a button that says pay with card
+
+  render() {
+    return (
+      <StripeCheckout token={this.onToken}
+      stripeKey='pk_test_XqkWNyas2OJODpoC8HRzwH5T' />
+    )
+  }
+}
---
+++ server/stripe/stripe.js
@@ -0,0 +1,5 @@
+const stripe = require('stripe')('pk_test_XqkWNyas2OJODpoC8HRzwH5T')
+
+//still a long way to go on this I think
+
+module.exports = stripe
