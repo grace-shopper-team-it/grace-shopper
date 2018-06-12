81bbf7ea9c86232faccc8121a173b353d1284a1c
aurre <aurrecochearaysa@gmail.com>
2018-06-12 13:23:54 -0500

    navbar css file added

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
+import './navbar.css';
 
 const Header = ({ handleClick, isLoggedIn, isAdmin }) => (
   <nav className="navbar navbar-light" style={{ backgroundColor: '#e3f2fd' }}>
