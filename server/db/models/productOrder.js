3f213670ce50e5d9deafd9f5ee264caec066314d
Chris <tophersjones@gmail.com>
2018-06-12 19:42:55 -0500

    inventory working, other stuff

--- server/db/models/productOrder.js
+++ server/db/models/productOrder.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const Product = require('./product')
 
 const ProductOrder = db.define('productOrder', {
   price: {
@@ -12,4 +13,17 @@ const ProductOrder = db.define('productOrder', {
   }
 });
 
-module.exports = ProductOrder
+//adjusts inventory of Product upon productOrder -cj
+ProductOrder.afterBulkCreate((productOrder) => {
+  productOrder.map( async (item) => {
+  const quant = item.cartQuantity;
+  const prodId = item.productId;
+  const product = await Product.findById(prodId);
+  product.inventory = product.inventory - quant;
+  console.log('PRODIUCTET', product);
+  product.save();
+});
+
+});
+
+module.exports = ProductOrder;
